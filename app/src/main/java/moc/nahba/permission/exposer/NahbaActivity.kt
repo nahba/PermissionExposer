b3ae76be2223165c3ef309f8ddfb52a23158a74b
Ketan <joomlaapk@gmail.com>
2021-04-04 18:39:09 +0530

    Initial commit

---
+++ app/src/main/java/moc/nahba/permission/exposer/NahbaActivity.kt
@@ -0,0 +1,11 @@
+package moc.nahba.permission.exposer
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class NahbaActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_nahba)
+    }
+}
